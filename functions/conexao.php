f067c733de211ea7457a95bee1280b0ffb638822
Renan <renan6rodrigues@outloook.com>
2023-04-17 22:18:03 -0300

    login no heroku

--- functions/conexao.php
+++ functions/conexao.php
@@ -1,6 +1,6 @@
 <?php
 function conectar(){
-    $strcon = mysqli_connect("localhost", "root", "", "central");
+    $strcon = mysqli_connect("@us-cdbr-east-06.cleardb.net", "b294236a090aa0", "0e1bc49b","heroku_addc7054e957021");
 	mysqli_set_charset($strcon,"utf8");
     if (!$strcon)
         die ('<script type="text/javascript"> alert("Erro de conexão com o banco de dados.");</script>');
@@ -8,7 +8,7 @@ function conectar(){
     return $strcon;
 }
 function desc(){
-    $strcon = mysqli_connect("localhost", "root", "", "central");
+    $strcon = mysqli_connect("@us-cdbr-east-06.cleardb.net", "b294236a090aa0", "0e1bc49b","heroku_addc7054e957021");
     mysqli_close($strcon);
    // echo 'desconectado!';
 }
